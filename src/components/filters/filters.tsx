4bb1635985c65d1bf70ff6aab53ae0cccef99419
KovOlga <switch.to.olga@yandex.ru>
2024-03-10 18:45:21 +0300

    filter on btn click

--- src/components/filters/filters.tsx
+++ src/components/filters/filters.tsx
@@ -1,49 +1,77 @@
-import { FormItem, FormLayoutGroup, NativeSelect } from "@vkontakte/vkui";
-import { ChangeEvent, FC } from "react";
-import { useAppSelector } from "../../hooks/hooks";
+import {
+  Button,
+  Div,
+  FormItem,
+  FormLayoutGroup,
+  NativeSelect,
+} from "@vkontakte/vkui";
+import { ChangeEvent, FC, useState } from "react";
+import { useAppDispatch, useAppSelector } from "../../hooks/hooks";
 import { RootState } from "../../types";
 import { getGroups } from "../../services/actions";
+import { parseHasFriends, parseIsClosed } from "../../utils/parse-filters";
 
 const Filters: FC = () => {
+  const dispatch = useAppDispatch();
   const colors = useAppSelector((store: RootState) => store.groups.colors);
+  const [filtersValues, setFiltersValues] = useState({
+    privacy: "all",
+    color: "any",
+    friends: "all",
+  });
   const onFilterChange = (e: ChangeEvent<HTMLSelectElement>) => {
-    console.log("valu", e.target.value);
-    console.log("name", e.target.id);
-    // getGroups({
-    //   privacy: "all",
-    //   color: "any",
-    //   friends: "all",
-    // });
+    setFiltersValues({ ...filtersValues, [e.target.id]: e.target.value });
+  };
+  const handleApplyFilters = () => {
+    dispatch(
+      getGroups({
+        isClosed: parseIsClosed(filtersValues.privacy),
+        color: filtersValues.color === "any" ? null : filtersValues.color,
+        hasFriends: parseHasFriends(filtersValues.friends),
+      })
+    );
   };
   return (
-    <FormLayoutGroup mode="horizontal">
-      <FormItem top="По типу приватности" htmlFor="privacy">
-        <NativeSelect onChange={onFilterChange} id="privacy">
-          <option value="all">Все</option>
-          <option value="closed">Закрытая</option>
-          <option value="opened">Открытая</option>
-        </NativeSelect>
-      </FormItem>
-      <FormItem top="По цвету аватарки" htmlFor="color">
-        <NativeSelect onChange={onFilterChange} id="color">
-          <option value="any">Любой</option>
-          {colors.map((color, index) => {
-            return (
-              <option key={index} value={color}>
-                {color}
-              </option>
-            );
-          })}
-        </NativeSelect>
-      </FormItem>
-      <FormItem top="По наличию друзей в группе" htmlFor="friends">
-        <NativeSelect onChange={onFilterChange} id="friends">
-          <option value="all">Все</option>
-          <option value="presence">Есть друзья в группе</option>
-          <option value="absence">Нет друзей в группе</option>
-        </NativeSelect>
-      </FormItem>
-    </FormLayoutGroup>
+    <>
+      <FormLayoutGroup mode="horizontal">
+        <FormItem top="По типу приватности" htmlFor="privacy">
+          <NativeSelect onChange={onFilterChange} id="privacy">
+            <option value="all">Все</option>
+            <option value="closed">Закрытая</option>
+            <option value="opened">Открытая</option>
+          </NativeSelect>
+        </FormItem>
+        <FormItem top="По цвету аватарки" htmlFor="color">
+          <NativeSelect onChange={onFilterChange} id="color">
+            <option value="any">Любой</option>
+            {colors.map((color, index) => {
+              return (
+                <option key={index} value={color}>
+                  {color}
+                </option>
+              );
+            })}
+          </NativeSelect>
+        </FormItem>
+        <FormItem top="По наличию друзей в группе" htmlFor="friends">
+          <NativeSelect onChange={onFilterChange} id="friends">
+            <option value="all">Все</option>
+            <option value="present">Есть друзья в группе</option>
+            <option value="absent">Нет друзей в группе</option>
+          </NativeSelect>
+        </FormItem>
+      </FormLayoutGroup>
+      <Div>
+        <Button
+          type="button"
+          onClick={handleApplyFilters}
+          size="m"
+          mode="secondary"
+        >
+          Применить
+        </Button>
+      </Div>
+    </>
   );
 };
 
---
+++ src/utils/parse-filters.ts
@@ -0,0 +1,23 @@
+export const parseIsClosed = (type: string) => {
+  switch (type) {
+    case "all":
+      return null;
+    case "closed":
+      return true;
+
+    case "opened":
+      return false;
+  }
+};
+
+export const parseHasFriends = (type: string) => {
+  switch (type) {
+    case "all":
+      return null;
+    case "present":
+      return true;
+
+    case "absent":
+      return false;
+  }
+};
